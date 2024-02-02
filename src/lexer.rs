4d5b2b1d761ddcb4c65ecaf8ad129616cbbd3384
HydrogenMacro <hydrogenmacro@gmail.com>
2024-02-02 16:34:21 -0500

    Update lexer.rs

---
+++ src/lexer.rs
@@ -0,0 +1,13 @@
+#[derive(Debug)]
+pub struct Lexer {
+    
+};
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Token {
+    
+}
+impl Lexer {
+    pub const fn new() -> Lexer {
+        return Lexer {};
+    }
+}
